149fadd7baaa0299c55dc982ad4e3ffd8a013a85
Chetna <139314214+IChetna23@users.noreply.github.com>
2024-07-23 12:15:11 +0530

    Delete src/java1 directory

--- src/java1/j1HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package java1;
-
-public class j1HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World.");
-    }
-    j1HelloWorld object = new j1HelloWorld();
-}
